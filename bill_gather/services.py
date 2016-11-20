07de8045b2cbf2fb2719119f54d88d74163cb154
Ari Hrannar Björnsson <arihrannar@gmail.com>
2016-11-20 08:50:45 +0100

    adding document reading

--- bill_gather/services.py
+++ bill_gather/services.py
@@ -25,9 +25,30 @@ def scrape_by_parliament_session_number(parliament_session_number):
         columns = bill_row.find_all('td')
         bill_number = int(columns[0].getText())
         bill_date = columns[1].getText().split('.')
-        bill_name = columns[2].getText()
+        bill_name_column = columns[2]
+        bill_name = bill_name_column.getText()
+        bill_name_link = bill_name_column.find('a')['href']
+        cron_logger.info('www.althingi.is'+bill_name_link)
+
         if bill_number not in bill_number_list:
             cron_logger.info('Created: '+ columns[0].getText())
+
+            bill_process_page = requests.get('www.althingi.is'+bill_name_link)
+            bill_process_content = bill_process_page.content
+            bill_process_soup = BeautifulSoup(bill_process_content, 'html.parser')
+            divs_tablewrap = bill_process_soup.find_all('div', 'tablewrap')
+            cron_logger.info(divs_tablewrap)
+            parliament_document_table_body = divs_tablewrap.find('tbody')
+            cron_logger.info(parliament_document_table_body)
+            parliament_document_rows = parliament_document_table_body.find_all('tr')
+
+            for row in parliament_document_rows:
+                columns = row.find_all('td')
+                document_date = columns[0].getText().split('.')
+                document_link_column =  columns[1]
+                document_link = document_link_column.find('a')['href']
+                cron_logger.info('www.althingi.is'+document_link)
+
             Bill.objects.create(number=bill_number,
                                 name=bill_name,
                                 session=parliament_session,
