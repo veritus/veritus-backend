138002be620c02ead4a072cf0291d98adacfcce5
Ari Hrannar Björnsson <arihrannar@gmail.com>
2017-01-08 16:48:47 +0100

    added Entity model at top to abstract from. Reset migrations to make the transition easier

---
+++ main/models.py
@@ -0,0 +1,12 @@
+from __future__ import unicode_literals
+
+from django.db import models
+
+
+class Entity(models.Model):
+    name = models.TextField()
+    created = models.DateTimeField(auto_now_add=True)
+    modified = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
--- politech_backend/settings.py
+++ politech_backend/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'promises',
     'tags',
     'rest_framework',
+    'main',
 
 ]
 
