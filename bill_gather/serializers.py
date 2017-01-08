99c4e22a5549b6d66fe706c0fb785d4ffc22a13f
Ari Hrannar Björnsson <arihrannar@gmail.com>
2017-01-08 15:10:58 +0100

    made parliament serializers for the bill webservice

--- bill_gather/serializers.py
+++ bill_gather/serializers.py
@@ -1,13 +1,50 @@
 from rest_framework import serializers, viewsets
-from .models import Bill
+from .models import Bill, ParliamentSession, Parliament
+
+
+class ParliamentSerializer(serializers.HyperlinkedModelSerializer):
+
+    class Meta:
+        model = Parliament
+        fields = ('name','id')
+
+
+class ParliamentViewSet(viewsets.ModelViewSet):
+    queryset = Parliament.objects.all()
+    serializer_class = ParliamentSerializer
+
+
+
+
+
+class ParliamentSessionSerializer(serializers.HyperlinkedModelSerializer):
+
+    parliament = ParliamentSerializer()
+
+    class Meta:
+        model = ParliamentSession
+        fields = ('session_number', 'parliament')
+
+
+class ParliamentSessionViewSet(viewsets.ModelViewSet):
+    queryset = ParliamentSession.objects.all()
+    serializer_class = ParliamentSessionSerializer
+
+
+
 
 
 class BillSerializer(serializers.HyperlinkedModelSerializer):
+
+    session = ParliamentSessionSerializer()
+
     class Meta:
         model = Bill
-        fields = ('name', 'description_link', 'created_date', 'number')
+        fields = ('name', 'description_link', 'created_date', 'number', 'id', 'session')
 
-# ViewSets define the view behavior.
+
 class BillViewSet(viewsets.ModelViewSet):
     queryset = Bill.objects.all()
     serializer_class = BillSerializer
+
+
--- politech_backend/urls.py
+++ politech_backend/urls.py
@@ -1,12 +1,15 @@
 from django.contrib import admin
 from django.conf.urls import url, include
 from rest_framework import routers
-from bill_gather.serializers import BillViewSet
+from bill_gather.serializers import BillViewSet, ParliamentSessionViewSet, ParliamentViewSet
 
 
 # Routers provide an easy way of automatically determining the URL conf.
 router = routers.DefaultRouter()
 router.register(r'bills', BillViewSet)
+router.register(r'parliament_session', ParliamentSessionViewSet)
+router.register(r'parliament', ParliamentViewSet)
+
 
 # Wire up our API using automatic URL routing.
 # Additionally, we include login URLs for the browsable API.
