a5bb7ad14e40a68589a0f4a2bbd76e1cb62bff78
Ari Hrannar Björnsson <arihrannar@gmail.com>
2017-04-25 20:42:31 +0200

    removed unneeded import

--- party/views.py
+++ party/views.py
@@ -1,10 +1,10 @@
-from django.shortcuts import render
 from rest_framework import status
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.response import Response
 from rest_framework import permissions
 from .models import Party
-from .serializers import PartySerializerRead, PartySerializerWrite
+from .serializers import PartySerializerRead
+
 
 @api_view(['GET'])
 @permission_classes((permissions.AllowAny,))
