150e9ecbc06f03f0606d88553afeb79614b94eee
Einhyrningur <Einhyrningur@users.noreply.github.com>
2017-04-26 10:16:53 +0200

    Update tests.py

--- district/tests.py
+++ district/tests.py
@@ -21,4 +21,7 @@ class DistrictAPIIcelandic(APITestCase):
         district.save()
         
     def test_get_all(self):
+        response = self.client.get('/api/v1/districts/')
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(len(response.data), 1)
         self.assertEqual(response.data[0]['name'], 'Reykjavík Suður')
