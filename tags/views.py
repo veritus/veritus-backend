b1ba7a08ea160b5fd04f7f016c9e65825f3439b1
Ari Hrannar Björnsson <arihrannar@gmail.com>
2017-04-28 19:32:24 +0200

    added get all tags webservice

--- tags/views.py
+++ tags/views.py
@@ -3,14 +3,20 @@ from rest_framework.decorators import api_view, permission_classes
 from rest_framework.response import Response
 
 from .models import Tag, CaseTags, PromiseTags
-from .serializers import PromiseSerializerRead, PromiseSerializerWrite, PromiseCaseSerializer, SuggestedPromiseCaseSerializer
+from .serializers import TagSerializer, CaseTagSerializer, PromiseTagSerializer
 from rest_framework.permissions import IsAuthenticated,
 
-@api_view(['POST'])
+@api_view(['GET', 'POST'])
 @permission_classes((IsAuthenticated,))
 def tag_list(request):
-    serializer = PromiseSerializerWrite(data=request.data)
-    if serializer.is_valid():
-        serializer.save()
-        return Response(serializer.data, status=status.HTTP_201_CREATED)
-    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    if request.method == 'GET':
+        all_tags = Tag.objects.all()
+        serializer = TagSerializer(all_tags, many=True)
+        return Response(serializer, status=status.HTTP_200_OK)
+    elif request.method == 'POST':
+        serializer = TagSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
