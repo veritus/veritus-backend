698a6638dfe8326036867a328f18058cf463121f
ragnar <ragnarhardarson90@gmail.com>
2017-06-06 19:18:44 +0200

    Politicians model added

--- politicians/models.py
+++ politicians/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from main.models import Entity
+from party.models import Party
+from district.models import District
 
 # Create your models here.
+class Politician(Entity):
+    party = models.ForeignKey(Party)
+    initials = models.TextField()
+    district = models.ForeignKey(District)
+    districtNumber = models.IntegerField()
