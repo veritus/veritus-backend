71b1a84f74a90234882ccc2086ca2dee5c98faa5
Ari Hrannar Björnsson <arihrannar@gmail.com>
2017-11-12 13:21:08 +0100

    Added parliament members to parliament model and added all parliament members to init data

--- src/parliament/models.py
+++ src/parliament/models.py
@@ -2,13 +2,15 @@ from django.db import models
 from main.models import Entity
 from politicians.models import Politician
 
+class ParliamentMember(Politician):
+    pass
+
 class Parliament(Entity):
     start_date = models.DateField()
     end_date = models.DateField()
+    parliament_members = models.ManyToManyField(ParliamentMember)
 
 class ParliamentSession(Entity):
     session_number = models.IntegerField()
     parliament = models.ForeignKey(Parliament)
 
-class ParliamentMember(Politician):
-    pass
