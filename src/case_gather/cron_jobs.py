0d4572ca8e25bfb899f1b8f432ce61fde78a0855
Ari Hrannar Björnsson <arihrannar@gmail.com>
2017-11-14 17:26:43 +0100

    number of parliament sessions looked at is now a configurable environment variable

--- src/case_gather/cron_jobs.py
+++ src/case_gather/cron_jobs.py
@@ -8,14 +8,15 @@ import main.sentryLogger as SentryLogger
 
 class GatherCases(CronJobBase):
     RUN_EVERY_MINS = int(os.environ["GATHER_CASES_CRON_TIME_SECONDS"])
-
+    
     schedule = Schedule(run_every_mins=RUN_EVERY_MINS)
     code = 'case_gather.GatherCases'    # a unique code
 
     def do(self):
         try:
             SentryLogger.warning('Starting case gather')
-            parliament_sessions = ParliamentSession.objects.all().order_by('-id')[:3]
+            PARLIAMENT_SESSIONS_TO_LOOK_AT = int(os.environ["PARLIAMENT_SESSIONS_TO_LOOK_AT"])
+            parliament_sessions = ParliamentSession.objects.all().order_by('-id')[:PARLIAMENT_SESSIONS_TO_LOOK_AT]
             for parliament_session in parliament_sessions:
                 CaseGatheringService.update_cases_by_session_number(parliament_session)
             SentryLogger.warning('Case gather done')
--- src/votes/cron_jobs.py
+++ src/votes/cron_jobs.py
@@ -8,14 +8,15 @@ import main.sentryLogger as SentryLogger
 
 class GatherVotes(CronJobBase):
     RUN_EVERY_MINS = int(os.environ["GATHER_VOTES_CRON_TIME_SECONDS"])
-
+    
     schedule = Schedule(run_every_mins=RUN_EVERY_MINS)
     code = 'votes.GatherVotes'    # a unique code
 
     def do(self):
         try:
             SentryLogger.warning('Starting votes gather')
-            parliament_sessions = ParliamentSession.objects.all().order_by('-id')[:3]
+            PARLIAMENT_SESSIONS_TO_LOOK_AT = int(os.environ["PARLIAMENT_SESSIONS_TO_LOOK_AT"])
+            parliament_sessions = ParliamentSession.objects.all().order_by('-id')[:PARLIAMENT_SESSIONS_TO_LOOK_AT]
             for parliament_session in parliament_sessions:
                 VoteServices.get_votes_by_parliament_session(parliament_session)
             SentryLogger.warning('Votes gather done')
